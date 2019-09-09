a775f9e8eb4c653e7a549ae4818c0ab53151c154
A Code Fox <heyacfox@gmail.com>
2019-09-09 19:24:00 -0400

    Tried to fix Packaging Issues

--- Assets/VuphoriaToKeep/DefaultTrackableEventHandlerBirds.cs
+++ Assets/VuphoriaToKeep/DefaultTrackableEventHandlerBirds.cs
@@ -17,7 +17,7 @@ using UnityEngine.Events;
 /// Changes made to this file could be overwritten when upgrading the Vuforia version.
 /// When implementing custom event handler behavior, consider inheriting from this class instead.
 /// </summary>
-public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+public class DefaultTrackableEventHandlerBirds : DefaultTrackableEventHandler
 {
 
     public TargetType targetType;
